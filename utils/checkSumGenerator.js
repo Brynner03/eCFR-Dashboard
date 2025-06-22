3cdbfa6405ccb7f9df13dc6d5caad9d6adbb3668
Brynner03 <bventura03@hotmail.com>
2025-06-22 19:54:46 -0400

    Improved checksum reliability by including additional agency data fields.

--- utils/checkSumGenerator.js
+++ utils/checkSumGenerator.js
@@ -3,7 +3,10 @@ const crypto = require("crypto")
 function checksumGenerator(agencyData) {
 
     const baseString = JSON.stringify({
+        display_name: agencyData.display_name,
+        sortable_name: agencyData.sortable_name,
         slug: agencyData.slug,
+        children: agencyData.children,
         cfr_references: agencyData.cfr_references,
     })
 
